80b37e9edea91cf9ecb50c5f56a50f9e6bbd9f28
nevah_evans <nevahevans@yahoo.com>
2024-06-26 20:51:13 -0500

    adding starter code

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 
 inquirer
@@ -7,3 +8,14 @@ inquirer
       name: 'enterKey'
     }
   ]);
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
